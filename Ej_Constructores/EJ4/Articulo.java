86dea32aafc39c3f236d6c0be6a4e1a8ac1afafc
Sam <sergimarenas@gmail.com>
2023-03-14 10:14:17 +0100

    Changes

--- Ej_Constructores/EJ4/Articulo.java
+++ Ej_Constructores/EJ4/Articulo.java
@@ -6,7 +6,31 @@ public class Articulo {
     double iva = 21;
     int cuantosQuedan;
 
-    public Articulo(String nombre, double precio, double iva, int cuantosQuedan) {
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+    public double getPrecio() {
+        return precio;
+    }
+    public void setIva(double iva) {
+        this.iva = iva;
+    }
+    public double getIva() {
+        return iva;
+    }
+    public void setCuantosQuedan(int cuantosQuedan) {
+        this.cuantosQuedan = cuantosQuedan;
+    }
+    public int getCuantosQuedan() {
+        return cuantosQuedan;
+    }
+    public Articulo(double precio, double iva, int cuantosQuedan) {
         if (iva > 0 && iva < 100)
             this.iva = iva;
         else
@@ -19,5 +43,14 @@ public class Articulo {
             this.cuantosQuedan = cuantosQuedan;
         else
             System.out.println("ERROR al instanciar el número de productos en el almacén");
+    }
+    public double getPVP() {
+        double precioPVP = precio * (iva / 100 + 1);
+        return precioPVP;
+    }
+    public void imprime() {
+        System.out.println("Nombre: " + nombre + " - " + "Precio: " + precio + "€");
+        System.out.println("IVA: " + iva + "%" + " - " + "PVP: " + getPVP() + "€");
+        System.out.println("Quedan: " + cuantosQuedan + " unidades");
     }
 }
--- Ej_Constructores/EJ4/UD6_ProgramaArticulo.java
+++ Ej_Constructores/EJ4/UD6_ProgramaArticulo.java
@@ -2,16 +2,8 @@ package EJ4;
 //CONSTRUCTOR Sergi Marrahy Arenas
 public class UD6_ProgramaArticulo {
     public static void main(String[] args) {
-        Articulo producto = new Articulo("Pijama", 15, 21, 127);
-        producto.nombre = "Pijama";
-        producto.precio = 15;
-        producto.cuantosQuedan = 127;
-
-        double precioPVP = producto.precio * (producto.iva / 100 + 1);
-
-        System.out.print(producto.nombre + " - ");
-        System.out.println("Precio: " + producto.precio + "€" + " - ");
-        System.out.print("IVA: " + producto.iva + "%" + " - ");
-        System.out.println("PVP: " + precioPVP + "€");
+        Articulo producto = new Articulo(11, 78, 11);
+        producto.setNombre("Pijama");
+        producto.imprime();
     }
 }
